fe9a2078f2b67f3fe7167dab219165bdc941f385
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-06 20:57:04 -0700

    Create wrapper package
    This is where all the classes that wrap the information about the API will live.

---
+++ src/main/java/com/insertign/pineapple/wrapper/player/Player.java
@@ -0,0 +1,46 @@
+package com.insertign.pineapple.wrapper.player;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class Player {
+
+    private JSONObject JSONPlayerObject;
+
+    public Player(JSONObject JSONPlayerObject) {
+        this.JSONPlayerObject = JSONPlayerObject;
+    }
+
+    public String getUsername() {
+        return null;
+    }
+
+    public String getUuid() {
+        return null;
+    }
+    public double getLevel() {
+        return 0;
+    }
+
+    public int getAchievementPoints() {
+        return 0;
+    }
+
+    public int getKarma() {
+        return 0;
+    }
+
+    public int getQuestsCompleted() {
+        return 0;
+    }
+
+    public List<Player> getFriendsList() {
+        return null;
+    }
+
+    public boolean isActive() {
+        return false;
+    }
+
+}
