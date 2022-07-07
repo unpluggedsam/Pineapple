d471ecbcd35ab735383063c37cc9dc382ecdee82
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-06 20:50:08 -0700

    Update Player.java
    Added skeleton methods that will return Player information.

--- src/main/java/com/insertign/pineapple/player/Player.java
+++ src/main/java/com/insertign/pineapple/player/Player.java
@@ -2,16 +2,45 @@ package com.insertign.pineapple.player;
 
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class Player {
 
     private JSONObject JSONPlayerObject;
-    
-    private String username;
-    private String uuid;
 
     public Player(JSONObject JSONPlayerObject) {
         this.JSONPlayerObject = JSONPlayerObject;
     }
 
+    public String getUsername() {
+        return null;
+    }
+
+    public String getUuid() {
+        return null;
+    }
+    public double getLevel() {
+        return 0;
+    }
+
+    public int getAchievementPoints() {
+        return 0;
+    }
+
+    public int getKarma() {
+        return 0;
+    }
+
+    public int getQuestsCompleted() {
+        return 0;
+    }
+
+    public List<Player> getFriendsList() {
+        return null;
+    }
+
+    public boolean isActive() {
+        return false;
+    }
 
 }
